5653d639e2ffef84611e86432189de7e6d7d4c85
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-01-12 08:00:46 +0100

    Adds yff-filter (minor)

--- test/lib/get-schools-test.js
+++ test/lib/get-schools-test.js
@@ -6,7 +6,12 @@ const getSchools = require('../../lib/get-schools')
 const options = {
   shortName: 'dalvs'
 }
+const yffOptions = {
+  yff: true
+}
 
 tap.equal(schools.length, getSchools().length, 'It returns all schools on empty')
 
 tap.equal(1, getSchools(options).length, 'It returns 1 school for dalvs')
+
+tap.equal(13, getSchools(yffOptions).length, 'It returns 13 schools for yff:true')
