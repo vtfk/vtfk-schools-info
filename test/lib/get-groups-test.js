5e15d433ee16d38faab55072a88e2528d620eb2f
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-06-29 11:08:21 +0200

    Removed old group stuff

--- test/lib/get-groups-test.js
+++
@@ -1,8 +0,0 @@
-const tap = require('tap')
-const groups = require('../../lib/data/groups.json')
-const getGroups = require('../../lib/get-groups')
-const id = 'dalvs'
-
-tap.equal(Object.keys(groups).length, Object.keys(getGroups()).length, 'It returns all groups on empty')
-
-tap.equal(26, getGroups(id).length, 'It returns 26 groups for dalvs')
